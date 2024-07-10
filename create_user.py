fc5257900ab1b87a6e23cd0cecf2a779fd8d7c09
Kirill Napitkin <napitkin@gmail.com>
2024-07-10 16:24:49 +0300

    Передача параметров в методы

--- create_user.py
+++ create_user.py
@@ -11,74 +11,90 @@ class CreateUser(unittest.TestCase):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    def test_create_user(self):
-        wd = self.wd
-        # open homepage
+    def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
-        # login
+
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
-        # open create user page
+
+    def open_page_create_user(self, wd):
         wd.find_element_by_link_text("add new").click()
+
+    def create_user(self, wd, first_name, middle_name, nick_name, title, company, address, tel_home, tel_mobile, tel_work, email, b_day, b_month, b_year, anniversary_day, anniversary_month,
+                    anniversary_year):
         # fill user form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("Kirill")
+        wd.find_element_by_name("firstname").send_keys(first_name)
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("Napitkin")
+        wd.find_element_by_name("middlename").send_keys(middle_name)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("Napitkin")
+        wd.find_element_by_name("nickname").send_keys(nick_name)
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys("Da")
+        wd.find_element_by_name("title").send_keys(title)
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("Rus_Company")
+        wd.find_element_by_name("company").send_keys(company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("Moscow Kremlin str.")
+        wd.find_element_by_name("address").send_keys(address)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("7777777")
+        wd.find_element_by_name("home").send_keys(tel_home)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("2222222222")
+        wd.find_element_by_name("mobile").send_keys(tel_mobile)
         wd.find_element_by_name("work").click()
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys("55555555")
+        wd.find_element_by_name("work").send_keys(tel_work)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("kirill@kirill.ru")
+        wd.find_element_by_name("email").send_keys(email)
         wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text("22")
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(b_day)
         wd.find_element_by_xpath("//option[@value='22']").click()
         wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text("August")
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(b_month)
         wd.find_element_by_xpath("//option[@value='August']").click()
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys("1986")
+        wd.find_element_by_name("byear").send_keys(b_year)
         wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text("10")
+        Select(wd.find_element_by_name("aday")).select_by_visible_text(anniversary_day)
         wd.find_element_by_xpath("//div[@id='content']/form/select[3]/option[12]").click()
         wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text("August")
+        Select(wd.find_element_by_name("amonth")).select_by_visible_text(anniversary_month)
         wd.find_element_by_xpath("//div[@id='content']/form/select[4]/option[9]").click()
         wd.find_element_by_name("ayear").click()
         wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys("2000")
+        wd.find_element_by_name("ayear").send_keys(anniversary_year)
         # click button 'Enter" - create user
         wd.find_element_by_xpath("//div[@id='content']/form/input[20]").click()
-        # return to homepage
-        wd.find_element_by_link_text("home page").click()
-        # logout
+
+    def return_to_homepage(self, wd):
+       wd.find_element_by_link_text("home page").click()
+
+    def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
-    
+
+    def test_create_user(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, "admin", "secret")
+        self.open_page_create_user(wd)
+        self.create_user(wd, "Kirill", "Napitkin", "Napitkin2", "Da", "Rus_Company", "Moscow Kremlin str.",
+                         "7777777", "2222222222", "55555555", "kirill@kirill.ru", "22", "August", "1986",
+                         "10", "August", "2000")
+        self.return_to_homepage(wd)
+        self.logout(wd)
+
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
