5098ae594fc18ecf8de51f13ffaec5a2013f3b32
Kirill Napitkin <napitkin@gmail.com>
2024-07-10 16:46:08 +0300

    Создание вспомогательного класса для хранения свойств и использование его в тестах для инициализации тестовых данных и для передачи этих данных во вспомогательные методы.

--- create_user.py
+++ create_user.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from user import User
 import unittest
 
 
@@ -26,55 +27,54 @@ class CreateUser(unittest.TestCase):
     def open_page_create_user(self, wd):
         wd.find_element_by_link_text("add new").click()
 
-    def create_user(self, wd, first_name, middle_name, nick_name, title, company, address, tel_home, tel_mobile, tel_work, email, b_day, b_month, b_year, anniversary_day, anniversary_month,
-                    anniversary_year):
+    def create_user(self, wd, User):
         # fill user form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(first_name)
+        wd.find_element_by_name("firstname").send_keys(User.first_name)
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(middle_name)
+        wd.find_element_by_name("middlename").send_keys(User.middle_name)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(nick_name)
+        wd.find_element_by_name("nickname").send_keys(User.nick_name)
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(title)
+        wd.find_element_by_name("title").send_keys(User.title)
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(company)
+        wd.find_element_by_name("company").send_keys(User.company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(address)
+        wd.find_element_by_name("address").send_keys(User.address)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(tel_home)
+        wd.find_element_by_name("home").send_keys(User.tel_home)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(tel_mobile)
+        wd.find_element_by_name("mobile").send_keys(User.tel_mobile)
         wd.find_element_by_name("work").click()
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(tel_work)
+        wd.find_element_by_name("work").send_keys(User.tel_work)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(email)
+        wd.find_element_by_name("email").send_keys(User.email)
         wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(b_day)
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(User.b_day)
         wd.find_element_by_xpath("//option[@value='22']").click()
         wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(b_month)
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(User.b_month)
         wd.find_element_by_xpath("//option[@value='August']").click()
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(b_year)
+        wd.find_element_by_name("byear").send_keys(User.b_year)
         wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text(anniversary_day)
+        Select(wd.find_element_by_name("aday")).select_by_visible_text(User.anniversary_day)
         wd.find_element_by_xpath("//div[@id='content']/form/select[3]/option[12]").click()
         wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text(anniversary_month)
+        Select(wd.find_element_by_name("amonth")).select_by_visible_text(User.anniversary_month)
         wd.find_element_by_xpath("//div[@id='content']/form/select[4]/option[9]").click()
         wd.find_element_by_name("ayear").click()
         wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(anniversary_year)
+        wd.find_element_by_name("ayear").send_keys(User.anniversary_year)
         # click button 'Enter" - create user
         wd.find_element_by_xpath("//div[@id='content']/form/input[20]").click()
 
@@ -89,9 +89,9 @@ class CreateUser(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, "admin", "secret")
         self.open_page_create_user(wd)
-        self.create_user(wd, "Kirill", "Napitkin", "Napitkin2", "Da", "Rus_Company", "Moscow Kremlin str.",
+        self.create_user(wd, User("Kirill", "Napitkin", "Napitkin2", "Da", "Rus_Company", "Moscow Kremlin str.",
                          "7777777", "2222222222", "55555555", "kirill@kirill.ru", "22", "August", "1986",
-                         "10", "August", "2000")
+                         "10", "August", "2000"))
         self.return_to_homepage(wd)
         self.logout(wd)
 
---
+++ user.py
@@ -0,0 +1,19 @@
+class User:
+    def __init__(self, first_name, middle_name, nick_name, title, company, address, tel_home, tel_mobile, tel_work, email, b_day, b_month, b_year, anniversary_day, anniversary_month,
+                    anniversary_year):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.nick_name = nick_name
+        self.title = title
+        self.company = company
+        self.address = address
+        self.tel_home = tel_home
+        self.tel_mobile = tel_mobile
+        self.tel_work = tel_work
+        self.email = email
+        self.b_day = b_day
+        self.b_month = b_month
+        self.b_year = b_year
+        self.anniversary_day = anniversary_day
+        self.anniversary_month = anniversary_month
+        self.anniversary_year = anniversary_year
