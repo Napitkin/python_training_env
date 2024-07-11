c24fa99e0e1e8a87c3cdade068a37267ce06cc8b
Kirill Napitkin <napitkin@gmail.com>
2024-07-11 12:16:50 +0300

    1) Реализовал функции для создания фикстуры; 2) Избавился от тестовых классов, сделал тесты обычными функциями, в которые фикстура передаётся в качестве параметра (ДЗ-4).

--- create_user.py
+++ create_user.py
@@ -1,109 +1,19 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 from class_user import User
-import unittest
+from application_create_user import Application_create_user
+import pytest
 
 
-class CreateUser(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_page_create_user(self):
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
-
-    def create_user(self, User):
-        wd = self.wd
-        self.open_page_create_user()
-        # fill user form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(User.first_name)
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(User.middle_name)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(User.nick_name)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(User.title)
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(User.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(User.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(User.tel_home)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(User.tel_mobile)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(User.tel_work)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(User.email)
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(User.b_day)
-        wd.find_element_by_xpath("//option[@value='22']").click()
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(User.b_month)
-        wd.find_element_by_xpath("//option[@value='August']").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(User.b_year)
-        wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text(User.anniversary_day)
-        wd.find_element_by_xpath("//div[@id='content']/form/select[3]/option[12]").click()
-        wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text(User.anniversary_month)
-        wd.find_element_by_xpath("//div[@id='content']/form/select[4]/option[9]").click()
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(User.anniversary_year)
-        # click button 'Enter" - create user
-        wd.find_element_by_xpath("//div[@id='content']/form/input[20]").click()
-        self.return_to_homepage()
-
-    def return_to_homepage(self):
-       wd = self.wd
-       wd.find_element_by_link_text("home page").click()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-
-    def test_create_user(self):
-        self.login("admin", "secret")
-        self.create_user(User("Kirill", "Napitkin", "Napitkin2", "Da", "Rus_Company", "Moscow Kremlin str.",
-                         "7777777", "2222222222", "55555555", "kirill@kirill.ru", "22", "August", "1986",
-                         "10", "August", "2000"))
-        self.logout()
+@pytest.fixture
+def app(request):
+    fixture = Application_create_user()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-    def tearDown(self):
-        self.wd.quit()
-
-
-if __name__ == "__main__":
-    unittest.main()
+def test_create_user(app):
+     app.login("admin", "secret")
+     app.create_user(User("Kirill", "Napitkin", "Napitkin2", "Da", "Rus_Company", "Moscow Kremlin str.",
+              "7777777", "2222222222", "55555555", "kirill@kirill.ru", "22", "August", "1986",
+              "10", "August", "2000"))
+     app.logout()
