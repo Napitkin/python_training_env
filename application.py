417aee3ca25a1c226c7ce7526489dc3dac3a8e75
Kirill Napitkin <napitkin@gmail.com>
2024-07-11 11:18:16 +0300

    Перевод тестов с unittest на py.test.

--- application.py
+++ application.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+
+
 class Application:
     def __init__(self):
         self.wd = webdriver.Firefox()
--- test_add_group.py
+++ test_add_group.py
@@ -1,87 +1,24 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
+import pytest
 from group import Group
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest
+from application import Application
 
 
-class TestAddGroup(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
-
-    def open_homepage(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_homepage()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        self.open_groups_page()
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
-
-    def return_to_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-
-    def test_add_group(self):
-        self.login("admin", "secret")
-        self.open_groups_page()
-        self.create_group(Group("Keks", "Keks1", "KeksGroup"))
-        self.logout()
-
-    def test_add_empty_group(self):
-        self.login("admin", "secret")
-        self.create_group(Group("", "", ""))
-        self.logout()
-
-    def is_element_present(self, how, what):
-        try:
-            self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-
-    def is_alert_present(self):
-        try:
-            self.wd.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
-
-    def tearDown(self):
-        self.wd.quit()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_group(app):
+    app.login("admin", "secret")
+    app.open_groups_page()
+    app.create_group(Group("Keks", "Keks1", "KeksGroup"))
+    app.logout()
+
+
+def test_add_empty_group(app):
+    app.login("admin", "secret")
+    app.create_group(Group("", "", ""))
+    app.logout()
