6cf25da12232ce6668dc7295cbe2f8195a41a244
Kirill Napitkin <napitkin@gmail.com>
2024-07-12 14:32:21 +0300

    Выделен класс 'SessionHelper'

--- test/test_create_user.py
+++ test/test_create_user.py
@@ -12,8 +12,8 @@ def app(request):
 
 
 def test_create_user(app):
-    app.login("admin", "secret")
+    app.session.login("admin", "secret")
     app.create_user(User("Kirill", "Napitkin", "Napitkin2", "Da", "Rus_Company", "Moscow Kremlin str.",
                          "7777777", "2222222222", "55555555", "kirill@kirill.ru", "22", "August", "1986",
                          "10", "August", "2000"))
-    app.logout()
+    app.session.logout()
