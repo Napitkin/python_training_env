ad49b9ad8e20af3a45e3ce52f0e52bc2415d210d
Kirill Napitkin <napitkin@gmail.com>
2024-07-12 11:31:52 +0300

    1) Удалил второй файл 'Application_create_user'; 2) Удалил второй класс 'Application_create_user'; 3) Рефакторинг кода.

--- fixture/application.py
+++ fixture/application.py
@@ -51,3 +51,61 @@ class Application:
 
     def destroy(self):
         self.wd.quit()
+
+    def open_page_create_user(self):
+        wd = self.wd
+        wd.find_element_by_link_text("add new").click()
+
+    def create_user(self, user):
+        wd = self.wd
+        self.open_page_create_user()
+        # fill user form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(user.first_name)
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(user.middle_name)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(user.nick_name)
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(user.title)
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(user.company)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(user.address)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(user.tel_home)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(user.tel_mobile)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(user.tel_work)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(user.email)
+        wd.find_element_by_name("bday").click()
+        wd.find_element_by_xpath("//option[@value='22']").click()
+        wd.find_element_by_name("bmonth").click()
+        wd.find_element_by_xpath("//option[@value='August']").click()
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(user.b_year)
+        wd.find_element_by_name("aday").click()
+        wd.find_element_by_xpath("//div[@id='content']/form/select[3]/option[12]").click()
+        wd.find_element_by_name("amonth").click()
+        wd.find_element_by_xpath("//div[@id='content']/form/select[4]/option[9]").click()
+        wd.find_element_by_name("ayear").click()
+        wd.find_element_by_name("ayear").clear()
+        wd.find_element_by_name("ayear").send_keys(user.anniversary_year)
+        # click button 'Enter" - create user
+        wd.find_element_by_xpath("//div[@id='content']/form/input[20]").click()
+        self.return_to_homepage()
+
+    def return_to_homepage(self):
+        wd = self.wd
+        wd.find_element_by_link_text("home page").click()
--- test/test_create_user.py
+++ test/test_create_user.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-from env.model.user import User
-from env.fixture.application_create_user import Application_create_user
+from model.user import User
+from fixture.application import Application
 import pytest
 
 
 @pytest.fixture
 def app(request):
-    fixture = Application_create_user()
+    fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
 
@@ -14,6 +14,6 @@ def app(request):
 def test_create_user(app):
     app.login("admin", "secret")
     app.create_user(User("Kirill", "Napitkin", "Napitkin2", "Da", "Rus_Company", "Moscow Kremlin str.",
-              "7777777", "2222222222", "55555555", "kirill@kirill.ru", "22", "August", "1986",
-              "10", "August", "2000"))
+                         "7777777", "2222222222", "55555555", "kirill@kirill.ru", "22", "August", "1986",
+                         "10", "August", "2000"))
     app.logout()
