8a03701c563365a0fcba99c499c49bc3dac1d80a
Kirill Napitkin <napitkin@gmail.com>
2024-07-30 12:41:32 +0300

    Реализованна проверка валидности фикстуры и проверка состояния сессии (ДЗ-8).

--- test/test_delete_user.py
+++ test/test_delete_user.py
@@ -1,4 +1,4 @@
 def test_delete_first_user(app):
-    app.session.login(username="admin", password="secret")
+    # app.session.login(username="admin", password="secret")
     app.user.delete_first_user()
-    app.session.logout()
+    # app.session.logout()
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,12 +2,12 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
-    app.session.login("admin", "secret")
+    # app.session.login("admin", "secret")
     app.group.modify_first_group(Group(name="New group"))
-    app.session.logout()
+    # app.session.logout()
 
 
 def test_modify_group_header(app):
-    app.session.login("admin", "secret")
+    # app.session.login("admin", "secret")
     app.group.modify_first_group(Group(header="New_header"))
-    app.session.logout()
+    # app.session.logout()
