c9c959f06ff4d16b854e15c70e3110910b66033b
Kirill Napitkin <napitkin@gmail.com>
2024-08-05 17:02:02 +0300

    1. Улучшил функции чтения информации о пользователе из тестируемого приложения; 2. Порезал строки на части, сделал тесты для проверки телефонов.

--- test/test_phones.py
+++ test/test_phones.py
@@ -7,6 +7,7 @@ def test_phones_on_home_page(app):
     assert user_from_home_page.tel_home == clear(user_from_edit_page.tel_home)
     assert user_from_home_page.tel_mobile == clear(user_from_edit_page.tel_mobile)
     assert user_from_home_page.tel_work == clear(user_from_edit_page.tel_work)
+    # assert user_from_view_page.tel_fax == user_from_edit_page.tel_fax
 
 
 def test_phones_on_user_view_page(app):
