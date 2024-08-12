3f1fff1512866d1a3808e2435f564ba30d56dfa3
Kirill Napitkin <napitkin@gmail.com>
2024-08-12 16:55:48 +0300

    Добавленна фикстура для взаимодействия с БД.

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 from fixture.application import Application
+from fixture.db import DbFixture
 import pytest
 import json
 import os.path
@@ -9,22 +10,37 @@ fixture = None
 target = None
 
 
+def load_config(file):
+    global target
+    if target is None:
+        # Путь к текущему файлу
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+        with open(config_file) as f:
+            target = json.load(f)
+    return target
+
+
 @pytest.fixture
 def app(request):
     global fixture
-    global target
     browser = request.config.getoption("--browser")
+    web_config = load_config(request.config.getoption("--target"))["web"]
     # Проверка, если файл не прочитан:
-    if target is None:
-        # Путь к текущему файлу
-        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
-        with open(config_file) as f:
-            target = json.load(f)
     if fixture is None or not fixture.is_valid():
         # Проверка, если файл прочитан:
-        fixture = Application(browser=browser, base_url=target["baseUrl"])
-    fixture.session.ensure_login(username=target["username"], password=target["password"])
+        fixture = Application(browser=browser, base_url=web_config["baseUrl"])
+    fixture.session.ensure_login(username=web_config["username"], password=web_config["password"])
     return fixture
+
+
+@pytest.fixture(scope="session")
+def db(request):
+    db_config = load_config(request.config.getoption("--target"))["db"]
+    dbfixture = DbFixture(host=db_config["host"], name=db_config["name"], user=db_config["user"], password=db_config["password"])
+    def fin():
+        dbfixture.destroy()
+    request.addfinalizer(fin)
+    return dbfixture
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -32,6 +48,7 @@ def stop(request):
     def fin():
         fixture.session.ensure_logout()
         fixture.destroy()
+
     request.addfinalizer(fin)
     return fixture
 
