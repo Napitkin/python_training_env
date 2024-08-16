3d79de8c13b41b2bc03567d5d0fc8b27c5377ae1
Kirill Napitkin <napitkin@gmail.com>
2024-08-16 12:30:53 +0300

    ДЗ-21 - Исправлен тест на модификацию пользователя.

--- test/test_modify_user.py
+++ test/test_modify_user.py
@@ -9,11 +9,12 @@ def test_modify_user(app, db, check_ui):
                  "nmail22222@mail.ru", "nmail_444444@mail.ru", "nmail_888888@mail.ru"))
     old_users = db.get_user_list()
     modify_user = random.choice(old_users)
-    modify_user.name = User("Kirill2", "Napitkin2", "Berlin Turtle str.", "77447773", "22224422", "55522555",
+    user = User("Kirill2", "Napitkin2", "Berlin Turtle str.", "77447773", "22224422", "55522555",
                             "kmail1111@mail.ru", "kmail_22222@mail.ru", "kmail_33333@mail.ru")
-    app.user.modify_user_by_id(modify_user, modify_user.id)
+    app.user.modify_user_by_id(user, modify_user.id)
+    # Прямо заменяем объект в списке
+    old_users[old_users.index(modify_user)] = user
     new_users = db.get_user_list()
     assert old_users == new_users
     if check_ui:
         assert sorted(new_users, key=User.id_or_max) == sorted(app.user.get_user_list(), key=User.id_or_max)
-
